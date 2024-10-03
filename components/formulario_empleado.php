ba78a4abfecad58d5d70b7e1e4600cf2cf782cf6
AlbertoRa09 <Xion507@gmail.com>
2024-10-03 18:45:22 -0500

    Nuevos_campos

--- components/formulario_empleado.php
+++ components/formulario_empleado.php
@@ -12,7 +12,7 @@
 </head>
 <body>
  <!-- Barra de navegacion que deberia ser rojo bebesones -->
-    <nav class="navbar navbar-expand-lg">
+ <nav class="navbar navbar-expand-lg">
         <div class="container-fluid">
             <a class="navbar-brand text-white" href="#">Puchaina88</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
@@ -39,6 +39,7 @@
     </nav>
 
     <!-- Formulario Perron -->
+     
     <div class="container">
         <div class="row justify-content-center">
             <div class="col-md-8">
@@ -63,6 +64,25 @@
                             <div class="col-md-6">
                                 <label for="apellido2" class="form-label">Segundo Apellido:</label>
                                 <input type="text" class="form-control" id="apellido2" name="apellido2" placeholder="Ingresa tu segundo apellido">
+                            </div>
+                        </div>
+                        
+                        <!-- Select para usar o no el apellido de casada -->
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label for="usa_apellidoC" class="form-label">¿Usa Apellido de Casada?</label>
+                                <select class="form-control" id="usa_apellidoC" name="usa_apellidoC" onchange="toggleApellidoCasada()">
+                                    <option value="no">No</option>
+                                    <option value="si">Sí</option>
+                                </select>
+                            </div>
+                        </div>
+                        
+                        <!-- Campo de Apellido de Casada, inicialmente oculto -->
+                        <div class="row mb-3" id="campo_apellidoC" style="display: none;">
+                            <div class="col-md-6">
+                                <label for="apellidoC" class="form-label">Apellido Casada:</label>
+                                <input type="text" class="form-control" id="apellidoC" name="apellidoC" placeholder="Ingresa tu apellido de casada">
                             </div>
                         </div>
                         <div class="mb-3">
@@ -103,13 +123,14 @@
                                 <input type="text" class="form-control" id="telefono" name="telefono" placeholder="Ingresa tu teléfono" required>
                             </div>
                         </div>
+                        <h3 class="text-center mb-4">Planilla</h3>
                         <div class="row mb-3">
                             <div class="col-md-6">
-                                <label for="salario_inicial" class="form-label">Salario Inicial:</label>
-                                <input type="text" class="form-control" id="salario_inicial" name="salario_inicial" placeholder="Ingresa tu salario inicial" required>
+                                <label for="horas_trabajadas" class="form-label">Horas trabajadas:</label>
+                                <input type="text" class="form-control" id="horas_trabajadas" name="horas_trabajadas" placeholder="Ingresa las horas trabajadas" required>
                             </div>
                             <div class="col-md-6">
-                                <label for="sal_hora" class="form-label">Salario por Hora:</label>
+                                <label for="sal_hora" class="form-label">Salario por hora:</label>
                                 <input type="text" class="form-control" id="sal_hora" name="sal_hora" placeholder="Ingresa tu salario por hora" required>
                             </div>
                         </div>
@@ -119,26 +140,46 @@
                                 <input type="text" class="form-control" id="salario_bruto" name="salario_bruto" placeholder="Ingresa tu salario bruto" required>
                             </div>
                             <div class="col-md-4">
-                                <label for="salario_neto" class="form-label">Salario Neto:</label>
-                                <input type="text" class="form-control" id="salario_neto" name="salario_neto" placeholder="Ingresa tu salario neto" required>
+                                <label for="seguro_social" class="form-label">Seguro Social:</label>
+                                <input type="text" class="form-control" id="seguro_social" name="seguro_social" placeholder="Ingresa el Seguro Social" required>
                             </div>
                             <div class="col-md-4">
-                                <label for="descuento" class="form-label">Descuento:</label>
-                                <input type="text" class="form-control" id="descuento" name="descuento" placeholder="Ingresa tu descuento" required>
+                                <label for="seguro_educativo" class="form-label">Seguro Educativo:</label>
+                                <input type="text" class="form-control" id="seguro_educativo" name="seguro_educativo" placeholder="Ingresa el Seguro Educativo" required>
+                            </div>
+                        </div>
+                        <div class="row mb-3">
+                            <div class="col-md-4">
+                                <label for="ir" class="form-label">1/R:</label>
+                                <input type="text" class="form-control" id="ir" name="ir" placeholder="Ingresa 1/R" required>
+                            </div>
+                            <div class="col-md-4">
+                                <label for="descuento1" class="form-label">Descuento1:</label>
+                                <input type="text" class="form-control" id="descuento1" name="descuento1" placeholder="Ingresa el Descuento1" required>
+                            </div>
+                            <div class="col-md-4">
+                                <label for="descuento2" class="form-label">Descuento2:</label>
+                                <input type="text" class="form-control" id="descuento2" name="descuento2" placeholder="Ingresa el Descuento2" required>
                             </div>
                         </div>
                         <div class="row mb-3">
                             <div class="col-md-6">
-                                <label for="bonos" class="form-label">Bonos:</label>
-                                <input type="text" class="form-control" id="bonos" name="bonos" placeholder="Ingresa tus bonos" required>
+                                <label for="descuento3" class="form-label">Descuento3:</label>
+                                <input type="text" class="form-control" id="descuento3" name="descuento3" placeholder="Ingresa el Descuento3">
                             </div>
                             <div class="col-md-6">
-                                <label for="num_planilla" class="form-label">Número de Planilla:</label>
-                                <input type="text" class="form-control" id="num_planilla" name="num_planilla" placeholder="Ingresa el número de planilla" required>
+                                <label for="deducciones" class="form-label">Deducciones:</label>
+                                <input type="text" class="form-control" id="deducciones" name="deducciones" readonly>
                             </div>
                         </div>
-                        <div class="d-grid gap-2 d-md-flex justify-content-md-end">
-                            <button type="submit" class="btn btn-primary">Registrar</button>
+                        <div class="row mb-3">
+                            <div class="col-md-6">
+                                <label for="salario_neto" class="form-label">Salario Neto:</label>
+                                <input type="text" class="form-control" id="salario_neto" name="salario_neto" readonly>
+                            </div>
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" class="btn btn-primary">Enviar</button>
                             <button type="reset" class="btn btn-secondary">Cancelar</button>
                         </div>
                     </form>
@@ -150,7 +191,17 @@
     <footer>
         <p>© 2024 Me Corro en los jefes.com Todos los derechos reservados.</p>
     </footer>
-
+<script>
+    function toggleApellidoCasada() {
+    var usaApellidoC = document.getElementById("usa_apellidoC").value;
+    var campoApellidoC = document.getElementById("campo_apellidoC");
+    if (usaApellidoC === "si") {
+        campoApellidoC.style.display = "block";
+    } else {
+        campoApellidoC.style.display = "none";
+    }
+}
+</script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
 </body>
 </html>
